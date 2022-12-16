11f5b529171bb02922b14767539f68db8e260fe3
KonstantinaStrantzali <konstrantz@gmail.com>
2022-12-16 11:06:21 +0000

    created navbar template and task page

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -6,16 +6,18 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-if os.path.exists("env.py"): 
+if os.path.exists("env.py"):
     import env  # noqa
 
 
-app = Flask(__name__) 
-# we create an instance of the imported Flask() class, and that will be stored in
-# a variable called 'app', which takes the default Flask __name__ module.
+app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
+
+from taskmanager import routes  # noqa
+
+
 
 from taskmanager import routes  # noqa
--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,4 +5,19 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("tasks.html")
+
+""" 
+create this 'taskmanager' database, navigate to the Terminal, and login to the Postgres CLI by typing 'psql' and hitting enter.
+To create the database, we can simply type: CREATE DATABASE taskmanager;
+Once that's created, we'll switch over to that connection by typing:
+\c taskmanager; We don't need to do anything else within the Postgres CLI now that we have the database
+created, so let's come out of the CLI by typing \q.
+It's important to note, that if you were to modify your models later, then you'll need
+to migrate these changes.
+Let's go ahead and access the Python interpreter by typing "python3" and enter.
+From here, we need to import our 'db' variable found within the taskmanager package, so type:
+from taskmanager import db
+Now, using db.create_all() method:
+
+"""
