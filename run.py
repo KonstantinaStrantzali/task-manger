f36f649d28e0e77daee737f1f61e17d98ffea824
KonstantinaStrantzali <konstrantz@gmail.com>
2022-12-16 09:41:00 +0000

    database schema created with postgres

--- run.py
+++ run.py
@@ -1,14 +1,10 @@
 import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-if os.path.exists("env.py"):
-    import env  # noqa
+from taskmanager import app
 
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-
-db = SQLAlchemy(app)
-
-from taskmanager import routes  # noqa
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
