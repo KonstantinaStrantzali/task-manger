955642c4f3ac5f307e5eec5720b5172c512aabea
KonstantinaStrantzali <konstrantz@gmail.com>
2022-12-17 09:04:11 +0000

    create edit functionality and page

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,10 +5,15 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
-
+"""
+Whenever we call this function by clicking the navbar link for Categories, it will query
+the database and retrieve all records from this table, then sort them by the category name.
+"""
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories) #first categories is the name is use
+    #in categoires.html template and the second is the variable defined above
 
 
 @app.route("/add_category", methods=["GET", "POST"])
@@ -19,6 +24,15 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
 
 """ 
 create this 'taskmanager' database, navigate to the Terminal, and login to the Postgres CLI by typing 'psql' and hitting enter.
